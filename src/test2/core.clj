158e36bd466a4e158989a0da7837721c225de872
Zakovryashin Vadim <zakovryashin.v@gmail.com>
2022-04-03 02:43:35 +0700

    Added some routes

--- src/test2/core.clj
+++ src/test2/core.clj
@@ -1,6 +1,45 @@
-(ns test2.core)
+(ns test2.core
+  (:gen-class)
+  (:require [compojure.core :as c]
+            [compojure.route :as route]
+            [ring.adapter.jetty :as jetty]
+            [ring.middleware.defaults :as ring-defaults]
+            [hiccup.core :as h]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defonce server (atom nil))
+
+(defn home-view [count]
+  [:html
+   [:body
+    [:h1 "Welcome home!"]
+    [:ul
+     (for [i (range count)]
+       [:li i])]]])
+
+(defn routes []
+  (c/routes
+    (c/GET "/" [count]
+      {:status  200
+       :headers {"Content-Type" "text/html"}
+       :body    (str (h/html (home-view (Integer. (or count 0)))))})
+    (c/GET "/:foo" [foo]
+      {:status 200
+       :body (str "you asked for " foo)})))
+
+(defn handler [req]
+  ((routes) req))
+
+(defn stop-jetty! []
+  (.stop @server)
+  (reset! server nil))
+
+(defn start-jetty! []
+  (reset!
+    server
+    (jetty/run-jetty (ring-defaults/wrap-defaults
+                       #'handler
+                       ring-defaults/site-defaults)
+                     {:join false :port 3001})))
+
+(defn -main [& args]
+  (start-jetty!))
